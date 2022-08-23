3d07c0b16de6f88cbd573af61c66ae1ee0a7421c
Mike <justeivin227@gmail.com>
2022-08-23 11:21:06 +0200

    Implement drawing

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const drawingBoard = document.getElementById('sketch-canvas');
-
 for(let i = 0; i < 9600; i++){
     let newPixel = document.createElement('div');
     newPixel.classList.add('pixel');
     drawingBoard.appendChild(newPixel);
 }
+
+const pixelsOnBoard = document.querySelectorAll('.pixel');
+
+
+let drawingMode = false;
+
+pixelsOnBoard.forEach((pixel) => {
+    pixel.addEventListener('mousedown', () => { 
+        drawingMode = true;
+      });
+      pixel.addEventListener('mouseup', () => { 
+        drawingMode = false;
+      });
+      pixel.addEventListener('mousemove', () => { 
+        if(drawingMode) {
+            pixel.style.backgroundColor = 'gray';
+        }
+      });
+});
