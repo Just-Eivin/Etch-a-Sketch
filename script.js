da8ee916a84312f3fec414dfdf5b4d406b3d583b
Mike <justeivin227@gmail.com>
2022-08-23 13:18:13 +0200

    Implement clearing algorithm

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const drawingBoard = document.getElementById('sketch-canvas');
-for(let i = 0; i < 9600; i++){
+for (let i = 0; i < 2368; i++) {
     let newPixel = document.createElement('div');
     newPixel.classList.add('pixel');
     drawingBoard.appendChild(newPixel);
@@ -11,15 +11,49 @@ const pixelsOnBoard = document.querySelectorAll('.pixel');
 let drawingMode = false;
 
 pixelsOnBoard.forEach((pixel) => {
-    pixel.addEventListener('mousedown', () => { 
+    pixel.addEventListener('mousedown', () => {
         drawingMode = true;
-      });
-      pixel.addEventListener('mouseup', () => { 
+    });
+    pixel.addEventListener('mouseup', () => {
         drawingMode = false;
-      });
-      pixel.addEventListener('mousemove', () => { 
-        if(drawingMode) {
-            pixel.style.backgroundColor = 'gray';
+    });
+    pixel.addEventListener('mousemove', () => {
+        if (drawingMode) {
+            pixel.classList.add('painted');
+
         }
-      });
+    });
+    pixel.addEventListener('click', () => {
+        pixel.classList.add('painted');
+    });
 });
+
+const shakeButton = document.querySelector('.shake-button');
+const sketchBoard = document.getElementById('sketch-container');
+
+shakeButton.addEventListener('click', () => {
+    clearAndShake();
+})
+
+function clearPixel(array) {
+    let randomPosition = Math.floor((Math.random() * array.length));
+    array[randomPosition].classList.remove('painted');
+}
+
+function clearAndShake() {
+    let i = document.querySelectorAll('.painted').length;
+    if(i == 0){
+        return;
+    }
+    sketchBoard.classList.add('shake');
+    let myInt = setInterval(() => {
+        let paintedPixels = document.querySelectorAll('.painted');
+        clearPixel(paintedPixels);
+        i--;
+        if(i == 0) {
+            clearInterval(myInt);
+            sketchBoard.classList.remove('shake');
+        }
+    }, 10);
+}
+
